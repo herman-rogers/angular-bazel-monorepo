45009cca8e07c30a1b6943fd3bec3bc3294dd6a9
Herman Rogers <herman.rogers@pm.me>
2022-02-16 11:07:05 -0500

    Update project one sass

--- projects/project_one/src/app/app.component.ts
+++ projects/project_one/src/app/app.component.ts
@@ -5,10 +5,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-component',
   template: `
-    <div>
-      <h1>Welcome to Project One</h1>
-      <project-one-dashboard></project-one-dashboard>
-    </div>
+    <mat-toolbar>
+      <button
+        mat-icon-button
+        [matMenuTriggerFor]="menu"
+        aria-label="Example icon-button with a menu"
+      >
+        <mat-icon>more_vert</mat-icon>
+      </button>
+
+      <mat-menu #menu="matMenu">
+        <button mat-menu-item>
+          <mat-icon>dialpad</mat-icon>
+          <span>Redial</span>
+        </button>
+        <button mat-menu-item disabled>
+          <mat-icon>voicemail</mat-icon>
+          <span>Check voice mail</span>
+        </button>
+        <button mat-menu-item>
+          <mat-icon>notifications_off</mat-icon>
+          <span>Disable alerts</span>
+        </button>
+      </mat-menu>
+
+      <span>Project One</span>
+    </mat-toolbar>
   `,
 })
 export class AppComponent {
